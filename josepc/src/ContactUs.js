7357cd3794f97e1e68af8cdeebf6407623cfa53f
jerson echeverri <jecheverri@abatechnology.com>
2023-06-11 15:51:51 -0500

    user information send data excel, slider services

--- josepc/src/ContactUs.js
+++ josepc/src/ContactUs.js
@@ -1,7 +1,116 @@
-import React from 'react';
+import React, { useState } from "react";
+import { TextField, Button, Typography, Container } from "@mui/material";
+import { IconButton } from "@mui/material";
+import WhatsAppIcon from "@mui/icons-material/WhatsApp";
 
-const ContactUs = () => {
-  return <h2>Contact Us Page</h2>;
-};
+function ContactForm() {
+  const [name, setName] = useState("");
+  const [phone, setPhone] = useState("");
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
 
-export default ContactUs;
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const formData = {
+      name,
+      phone,
+      email,
+      message: message.substring(0, 500), // Limit message to 500 characters
+    };
+
+    try {
+      const response = await fetch(
+        "https://script.google.com/macros/s/AKfycbzb5X4_hlc48yPIrxidnD--i4_LFqm-bwGdteHf7zHsjYBQc66ej3dm2nCF0aIxhCc1/exec",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(formData),
+        }
+      );
+
+      if (response.ok) {
+        alert("Data sent successfully!");
+        // Reset form fields
+        setName("");
+        setPhone("");
+        setEmail("");
+        setMessage("");
+      } else {
+        alert("Error sending data. Please try again later.");
+      }
+    } catch (error) {
+      console.error(error);
+      alert("Error sending data. Please try again later.");
+    }
+  };
+
+  const handleClick = () => {
+    const phoneNumber = "+573014109359";
+    const message = encodeURIComponent("Hello, I have a question.");
+
+    const url = `https://wa.me/${phoneNumber}?text=${message}`;
+    window.open(url, "_blank");
+  };
+
+  return (
+    <Container maxWidth="sm" sx={{ marginTop: "5%", marginBottom: "15%" }}>
+      <Typography variant="h5" align="center" gutterBottom>
+        Contact Form
+      </Typography>
+      <form onSubmit={handleSubmit}>
+        <TextField
+          label="Full Name"
+          variant="outlined"
+          fullWidth
+          margin="normal"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+          required
+        />
+        <TextField
+          label="Phone Number"
+          variant="outlined"
+          type="number"
+          fullWidth
+          margin="normal"
+          value={phone}
+          onChange={(e) => setPhone(e.target.value)}
+          required
+        />
+        <TextField
+          label="Email"
+          variant="outlined"
+          fullWidth
+          margin="normal"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          required
+        />
+        <TextField
+          label="Message (max 500 characters)"
+          variant="outlined"
+          multiline
+          rows={4}
+          fullWidth
+          margin="normal"
+          value={message}
+          onChange={(e) => setMessage(e.target.value)}
+          inputProps={{ maxLength: 500 }}
+          required
+        />
+        <Button type="submit" variant="contained" color="primary">
+          Send Data
+        </Button>
+      </form>
+      <IconButton color="primary" onClick={handleClick} sx={{ width: '100%'}}>
+        <Typography sx={{margin: '0 2%'}} color={'black'}>Mensaje directo via whatasapp click aqui</Typography>
+        <WhatsAppIcon color="success" />
+      </IconButton>
+    </Container>
+  );
+}
+
+export default ContactForm;
--- josepc/src/Services.js
+++ josepc/src/Services.js
@@ -1,16 +1,47 @@
 import {
   Box,
   Container,
+  ImageList,
+  ImageListItem,
+  ImageListItemBar,
   List,
   ListItem,
   ListItemIcon,
+  Paper,
   Typography,
 } from "@mui/material";
 import React, { useEffect } from "react";
 import video from "./assets/video/video_instJosePc.mp4";
 import { CheckCircle } from "@mui/icons-material";
+import alquiler from "./assets/img/alquiler.jpeg";
+import servicioTecnico from "./assets/img/Servivio Técnico especializado.jpeg";
+import venta from "./assets/img/venta.jpeg";
+import cctv from "./assets/img/cctv.jpeg";
 
 const Services = () => {
+  const itemData = [
+    {
+      img: cctv,
+      title:
+        "Venta e instalación garantizada con calidad y responsabilidad de sistemas modernos de vigilancia y seguridad CCTV, equipos de ultima generación, con control via celular, cámaras de última tecnología adecuada a cada necesidad.",
+    },
+    {
+      img: venta,
+      title:
+        "Te ofrecemos equipos y suministros de todas las marcas, adecuados a tus necesidades según el uso, asesoria completa y con la mayor garantía",
+    },
+    {
+      img: servicioTecnico,
+      title:
+        "Experiencia, responsabilidad y garantía en nuestro servicio técnico, asesoria y acompañamiento, repuestos de primera calidad garantizados y apoyo presencial o remoto, según el caso",
+    },
+    {
+      img: alquiler,
+      title:
+        "Venta e instalación garantizada con calidad y responsabilidad de sistemas modernos de vigilancia y seguridad CCTV, equipos de ultima generación, con control via celular, cámaras de última tecnología adecuada a cada necesidad.",
+    },
+  ];
+
   useEffect(() => {
     const videoElement = document.getElementById("video");
 
@@ -20,156 +51,190 @@ const Services = () => {
   }, []);
 
   return (
-    <Container maxWidth="md" sx={{ marginTop: "40px", marginBottom: "40px" }}>
-      <Box
-        sx={{ display: "flex", alignItems: "center", justifyContent: "center", width: '1000px' }}
-      >
-        <Box sx={{ flex: "1" }}>
-          <Typography
-            variant="h2"
-            sx={{ marginBottom: "20px", fontSize: "3em" }}
-          >
-            Nuestros Servicios
-          </Typography>
-          <List sx={{ marginBottom: "40px" }}>
-            <ListItem>
-              <ListItemIcon>
-                <CheckCircle />
-              </ListItemIcon>
-              <Typography
-                variant="body1"
-                component="span"
-                sx={{ fontWeight: "bold" }}
-              >
-                Atención personalizada a los clientes
-              </Typography>
-            </ListItem>
-            <ListItem>
-              <ListItemIcon>
-                <CheckCircle />
-              </ListItemIcon>
-              <Typography
-                variant="body1"
-                component="span"
-                sx={{ fontWeight: "bold" }}
-              >
-                Servicio técnico remoto seguro y garantizado
-              </Typography>
-            </ListItem>
-            <ListItem>
-              <ListItemIcon>
-                <CheckCircle />
-              </ListItemIcon>
-              <Typography
-                variant="body1"
-                component="span"
-                sx={{ fontWeight: "bold" }}
-              >
-                Responsabilidad y cumplimiento
-              </Typography>
-            </ListItem>
-            <ListItem>
-              <ListItemIcon>
-                <CheckCircle />
-              </ListItemIcon>
-              <Typography
-                variant="body1"
-                component="span"
-                sx={{ fontWeight: "bold" }}
-              >
-                Profesionales calificados
-              </Typography>
-            </ListItem>
-            <ListItem>
-              <ListItemIcon>
-                <CheckCircle />
-              </ListItemIcon>
-              <Typography
-                variant="body1"
-                component="span"
-                sx={{ fontWeight: "bold" }}
-              >
-                Tecnología de vanguardia
-              </Typography>
-            </ListItem>
-            <ListItem>
-              <ListItemIcon>
-                <CheckCircle />
-              </ListItemIcon>
-              <Typography
-                variant="body1"
-                component="span"
-                sx={{ fontWeight: "bold" }}
-              >
-                Instalaciones y equipos con garantía plena
-              </Typography>
-            </ListItem>
-            <ListItem>
-              <ListItemIcon>
-                <CheckCircle />
-              </ListItemIcon>
-              <Typography
-                variant="body1"
-                component="span"
-                sx={{ fontWeight: "bold" }}
-              >
-                Precios acorde al mercado
-              </Typography>
-            </ListItem>
-            <ListItem>
-              <ListItemIcon>
-                <CheckCircle />
-              </ListItemIcon>
-              <Typography
-                variant="body1"
-                component="span"
-                sx={{ fontWeight: "bold" }}
-              >
-                Alquiler de equipos de cómputo
-              </Typography>
-            </ListItem>
-            <ListItem>
-              <ListItemIcon>
-                <CheckCircle />
-              </ListItemIcon>
-              <Typography
-                variant="body1"
-                component="span"
-                sx={{ fontWeight: "bold" }}
-              >
-                Cámaras y sistemas CCTV adaptables a su necesidad
-              </Typography>
-            </ListItem>
-          </List>
-        </Box>
-        <Box sx={{ flex: "1" }}>
-          <Box
-            sx={{
-              position: "relative",
-              paddingBottom: "56.25%",
-              height: 0,
-              overflow: "hidden",
-              backgroundColor: "#000",
-            }}
-          >
-            <video
-              id="video"
-              src={video}
-              controls
-              autoPlay
-              muted="muted"
-              style={{
-                position: "absolute",
-                top: 0,
-                left: 0,
-                width: "100%",
-                height: "100%",
+    <>
+      <Container maxWidth="md" sx={{ marginTop: "40px", marginBottom: "40px" }}>
+        <Box
+          sx={{
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+            width: "1000px",
+          }}
+        >
+          <Box sx={{ flex: "1" }}>
+            <Typography
+              variant="h2"
+              sx={{ marginBottom: "20px", fontSize: "3em" }}
+            >
+              Nuestros Servicios
+            </Typography>
+            <List sx={{ marginBottom: "40px" }}>
+              <ListItem>
+                <ListItemIcon>
+                  <CheckCircle />
+                </ListItemIcon>
+                <Typography
+                  variant="body1"
+                  component="span"
+                  sx={{ fontWeight: "bold" }}
+                >
+                  Atención personalizada a los clientes
+                </Typography>
+              </ListItem>
+              <ListItem>
+                <ListItemIcon>
+                  <CheckCircle />
+                </ListItemIcon>
+                <Typography
+                  variant="body1"
+                  component="span"
+                  sx={{ fontWeight: "bold" }}
+                >
+                  Servicio técnico remoto seguro y garantizado
+                </Typography>
+              </ListItem>
+              <ListItem>
+                <ListItemIcon>
+                  <CheckCircle />
+                </ListItemIcon>
+                <Typography
+                  variant="body1"
+                  component="span"
+                  sx={{ fontWeight: "bold" }}
+                >
+                  Responsabilidad y cumplimiento
+                </Typography>
+              </ListItem>
+              <ListItem>
+                <ListItemIcon>
+                  <CheckCircle />
+                </ListItemIcon>
+                <Typography
+                  variant="body1"
+                  component="span"
+                  sx={{ fontWeight: "bold" }}
+                >
+                  Profesionales calificados
+                </Typography>
+              </ListItem>
+              <ListItem>
+                <ListItemIcon>
+                  <CheckCircle />
+                </ListItemIcon>
+                <Typography
+                  variant="body1"
+                  component="span"
+                  sx={{ fontWeight: "bold" }}
+                >
+                  Tecnología de vanguardia
+                </Typography>
+              </ListItem>
+              <ListItem>
+                <ListItemIcon>
+                  <CheckCircle />
+                </ListItemIcon>
+                <Typography
+                  variant="body1"
+                  component="span"
+                  sx={{ fontWeight: "bold" }}
+                >
+                  Instalaciones y equipos con garantía plena
+                </Typography>
+              </ListItem>
+              <ListItem>
+                <ListItemIcon>
+                  <CheckCircle />
+                </ListItemIcon>
+                <Typography
+                  variant="body1"
+                  component="span"
+                  sx={{ fontWeight: "bold" }}
+                >
+                  Precios acorde al mercado
+                </Typography>
+              </ListItem>
+              <ListItem>
+                <ListItemIcon>
+                  <CheckCircle />
+                </ListItemIcon>
+                <Typography
+                  variant="body1"
+                  component="span"
+                  sx={{ fontWeight: "bold" }}
+                >
+                  Alquiler de equipos de cómputo
+                </Typography>
+              </ListItem>
+              <ListItem>
+                <ListItemIcon>
+                  <CheckCircle />
+                </ListItemIcon>
+                <Typography
+                  variant="body1"
+                  component="span"
+                  sx={{ fontWeight: "bold" }}
+                >
+                  Cámaras y sistemas CCTV adaptables a su necesidad
+                </Typography>
+              </ListItem>
+            </List>
+          </Box>
+          <Box sx={{ flex: "1" }}>
+            <Box
+              sx={{
+                position: "relative",
+                paddingBottom: "56.25%",
+                height: 0,
+                overflow: "hidden",
+                backgroundColor: "#000",
               }}
-            ></video>
+            >
+              <video
+                id="video"
+                src={video}
+                controls
+                autoPlay
+                muted="muted"
+                style={{
+                  position: "absolute",
+                  top: 0,
+                  left: 0,
+                  width: "100%",
+                  height: "100%",
+                }}
+              ></video>
+            </Box>
           </Box>
         </Box>
-      </Box>
-    </Container>
+      </Container>
+      <ImageList
+        sx={{ width: "80%", margin: "5% auto 15% auto", overflowY: "unset" }}
+        cols={2}
+      >
+        {itemData.map((item) => (
+          <ImageListItem
+            key={item.img}
+            sx={{ aspectRatio: "1", width: "80%", margin: "3% 0" }}
+          >
+            <img
+              src={`${item.img}?w=248&fit=crop&auto=format`}
+              srcSet={`${item.img}?w=248&fit=crop&auto=format&dpr=2 2x`}
+              alt={item.title}
+              loading="lazy"
+              style={{ objectFit: "cover", width: "100%", height: "100%" }}
+            />
+            <Box sx={{ position: "relative", textAlign: "center" }}>
+              <Typography
+                variant="body2"
+                sx={{ maxWidth: "100%", textAlign: "left", fontWeight: "bold", fontSize: '1.2em' }}
+              >
+                {item.title}
+              </Typography>
+            </Box>
+          </ImageListItem>
+        ))}
+      </ImageList>
+    </>
   );
 };
 
